9858d5c8124d97b6841227dde539360f02aedba9
grishinrv <roman1988grishin@gmail.com>
2020-01-18 10:48:04 +0300

    Pluged in bpopup

---
+++ Web/Scripts/Scripts.js
@@ -0,0 +1,13 @@
+;(function($) {
+     // DOM Ready
+    $(function() {
+        // Binding a click event
+        // From jQuery v.1.7.0 use .on() instead of .bind()
+        $('#my-button').bind('click', function(e) {
+            // Prevents the default action to be triggered. 
+            e.preventDefault();
+            // Triggering bPopup when click event is fired
+            $('#pop_up').bPopup();
+        });
+    });
+})(jQuery);
